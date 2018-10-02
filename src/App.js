540d6d09c06139bb8b0e10360710d3052d1f5251
Jordan Kline <jordan.kline@gmail.com>
2018-10-02 11:31:19 -0700

    added css and index info from V1

--- src/App.js
+++ src/App.js
@@ -1,28 +1,45 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { connect } from 'react-redux';
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import TitleBar from './components/TitleBar.js';
+import QuoteContainer from './containers/QuoteContainer';
+import MeditationSessionsContainer from './containers/MeditationSessionsContainer';
+import TimerContainer from './containers/TimerContainer';
+import UserSelectContainer from './containers/UserSelectContainer';
+import ResourcesContainer from './containers/ResourcesContainer';
+import SketchBoard from './components/SketchBoard';
 import './App.css';
 
 class App extends Component {
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="App">
+          <TitleBar />
+          <BrowserRouter>
+            <div className="App-navbar">
+              <Link to="/" className="menuTile">Home</Link>
+              <Link to="/meditation_sessions" className="menuTile">Sessions</Link>
+              <Link to="/timer" className="menuTile">Timer</Link>
+              <Link to="/resources" className="menuTile">Resources</Link>
+              <Link to="/sketchboard" className="menuTile"></Link>
+              <Route exact path="/" component={UserSelectContainer} />
+              <Route exact path="/meditation_sessions" component={MeditationSessionsContainer} />
+              <Route path="/timer" component={TimerContainer} />
+              <Route path="/resources" component={ResourcesContainer} />
+              <Route path="/sketchboard" component={SketchBoard} />
+            </div>
+          </BrowserRouter>
+        <QuoteContainer />
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    currentUser: state.usersReducer.currentUser,
+  }
+}
+
+export default connect(mapStateToProps, null)(App);
