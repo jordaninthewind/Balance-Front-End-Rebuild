f4e86c5bd86973609e320a27fb5413194c4ce6b9
Jordan Kline <jordan.kline@gmail.com>
2020-03-25 20:40:19 -0700

    added action to timer and meditation_session reducer for adding session

--- src/reducers/meditationSessionsReducer.js
+++ src/reducers/meditationSessionsReducer.js
@@ -1,8 +1,10 @@
+// todo: build out better error handling
+
 const graphQlFetch = query => {
   return fetch("https://balance-graphql-backend.herokuapp.com/v1/graphql", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ query: query })
+    body: JSON.stringify({ query })
   });
 };
 
@@ -22,7 +24,32 @@ export const setLoading = () => {
   return { type: "SET_LOADING" };
 };
 
+const removeMeditationSession = session => {
+  return { type: "REMOVE_MEDITATION_SESSION", session };
+};
+
+export const saveUserMeditationSession = (currentUser, duration) => dispatch => {
+  console.log(currentUser)
+  console.log(duration)
+  // debugger;
+  const query = `
+  mutation AddMeditationSession {
+    insert_meditation_sessions(objects: {duration: 1000, user_id: 10}) {
+      returning {
+        id
+      }
+    }
+  }
+  `
+  graphQlFetch(query)
+    // .then(() => alert("Saved session!"))
+    .then(res => console.log(res))
+    .then(res => this.toggleModal())
+    .catch(res => console.log(res));
+}
+
 export const getUserMeditationSessions = currentUser => dispatch => {
+  // todo: update userId to firebase userid in state
   const userId = 10;
   const query = `
   query GetMeditationSessions {
@@ -41,20 +68,29 @@ export const getUserMeditationSessions = currentUser => dispatch => {
     .then(json => {
       dispatch(setMeditationSessions(json.data.meditation_sessions));
     })
-    .catch(err => console.log(err))
-    ;
-};
-
-const removeMeditationSession = session => {
-  return { type: "REMOVE_MEDITATION_SESSION", session };
+    .catch(err => console.log(err));
 };
 
 export const deleteMeditationSession = (currentUser, session) => dispatch => {
-  fetch(`${BASE_URL}/users/${currentUser.id}/meditation_sessions/${session}`, {
-    method: "DELETE"
-  }).then(() => {
-    dispatch(removeMeditationSession(session));
-  });
+  const query = `
+  mutation DeleteMeditationSession {
+    delete_meditation_sessions(where: {id: {_eq: ${session}}}) {
+      returning {
+        id
+      }
+    }
+  }  
+  `;
+
+  graphQlFetch(query)
+    .then(res => res.json())
+    .then(json => {
+      if (json.errors) throw new Error('this a problem')
+      dispatch(removeMeditationSession(session));
+    })
+    .catch(err => {
+      console.log(err);
+    });
 };
 
 // reducer
