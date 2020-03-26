f673ea56e5900f0a2c1a4a210359661242d40256
Jordan Kline <jordan.kline@gmail.com>
2020-03-25 17:24:58 -0700

    added required packages and updated get thunk to gql query

--- src/reducers/meditationSessionsReducer.js
+++ src/reducers/meditationSessionsReducer.js
@@ -1,35 +1,57 @@
+const graphQlFetch = query => {
+  return fetch("https://balance-graphql-backend.herokuapp.com/v1/graphql", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ query: query })
+  });
+};
+
 const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 // actions
 
 const setMeditationSessions = meditationSessions => {
-  return { type: 'GET_USER_MEDITATION_SESSIONS', meditationSessions };
+  return { type: "GET_USER_MEDITATION_SESSIONS", meditationSessions };
 };
 
 export const resetMeditationSessions = () => {
-  return { type: 'RESET_USER_MEDITATION_SESSIONS' };
+  return { type: "RESET_USER_MEDITATION_SESSIONS" };
 };
 
 export const setLoading = () => {
-  return { type: 'SET_LOADING' };
+  return { type: "SET_LOADING" };
 };
 
 export const getUserMeditationSessions = currentUser => dispatch => {
+  const userId = 10;
+  const query = `
+  query GetMeditationSessions {
+    meditation_sessions(where: {user_id: {_eq: ${userId}}}) {
+      duration
+      date: time_started
+      id
+    }
+  }
+  `;
+
   dispatch(setLoading());
-  fetch(`${BASE_URL}/users/${currentUser.id}/meditation_sessions.json`)
+
+  graphQlFetch(query)
     .then(res => res.json())
     .then(json => {
-      dispatch(setMeditationSessions(json));
-    });
+      dispatch(setMeditationSessions(json.data.meditation_sessions));
+    })
+    .catch(err => console.log(err))
+    ;
 };
 
 const removeMeditationSession = session => {
-  return { type: 'REMOVE_MEDITATION_SESSION', session };
+  return { type: "REMOVE_MEDITATION_SESSION", session };
 };
 
 export const deleteMeditationSession = (currentUser, session) => dispatch => {
   fetch(`${BASE_URL}/users/${currentUser.id}/meditation_sessions/${session}`, {
-    method: 'DELETE'
+    method: "DELETE"
   }).then(() => {
     dispatch(removeMeditationSession(session));
   });
@@ -47,23 +69,23 @@ export default function meditationSessionsReducer(
   action
 ) {
   switch (action.type) {
-    case 'SET_LOADING':
+    case "SET_LOADING":
       return {
         ...state,
-        loading: true,
-      }
-    case 'GET_USER_MEDITATION_SESSIONS':
+        loading: true
+      };
+    case "GET_USER_MEDITATION_SESSIONS":
       return {
         ...state,
         meditationSessions: action.meditationSessions,
         loading: false
       };
-    case 'RESET_USER_MEDITATION_SESSIONS':
+    case "RESET_USER_MEDITATION_SESSIONS":
       return {
         ...state,
         meditationSessions: []
       };
-    case 'REMOVE_MEDITATION_SESSION':
+    case "REMOVE_MEDITATION_SESSION":
       return {
         ...state,
         meditationSessions: state.meditationSessions.filter(sess => {
