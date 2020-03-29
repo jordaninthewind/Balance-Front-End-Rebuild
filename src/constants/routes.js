f64f9fc771d632941ef7b6390cdff66626f1957f
Jordan Kline <jordankline@Jordans-MacBook-Air.local>
2020-03-29 14:38:37 -0700

    made external routes file for constants

---
+++ src/constants/routes.js
@@ -0,0 +1,9 @@
+export const LANDING = '/';
+export const SIGN_UP = '/signup';
+export const SIGN_IN = '/signin';
+export const ACCOUNT = '/account';
+export const ADMIN = '/admin';
+export const PASSWORD_FORGET = '/pw-forget';
+export const TIMER = '/timer';
+export const MEDITATION_SESSIONS = '/meditation_sessions';
+export const RESOURCES = '/resources';
