01ee86288f6bcb15aa4d7e44bdabe0a8c9a1f089
jordaninthewind <jordan.kline@gmail.com>
2020-06-08 17:03:19 -0700

    Removed bootstrap from resources

--- src/components/ResourceTile/ResourceTile.js
+++ src/components/ResourceTile/ResourceTile.js
@@ -1,31 +1,36 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
 import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
-import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
 // import "./ResourceTile.scss";
 
 const useStyles = makeStyles({
   root: {
-    maxWidth: 345,
+    maxWidth: 200,
   },
   media: {
-    height: 140,
+    height: 120,
+    width: 120
   },
 });
 
 const ResourceTile = ({ image, link, title }) => {
+  const classes = useStyles();
   return (
-    <Card>
-      <CardActionArea>
-        <CardMedia image={image} alt="" />
-        <CardContent>
-          <a href={link}>{title}</a>
-        </CardContent>
-      </CardActionArea>
+    <Card className={classes.root}>
+      <a href={link} target="_blank" noopener noreferral style={{textDecoration: 'none'}}>
+        <CardActionArea>
+          <CardMedia image={image} className={classes.media} alt="" />
+          <CardContent>
+            <Typography variant="h5" color="primary">{title}</Typography>
+          </CardContent>
+        </CardActionArea>
+      </a>
     </Card>
   );
 };
